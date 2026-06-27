fe7a2f783ce53128f52c610b6103c20b8abc4306
Prarthana <prarthanas3310@gmail.com>
2026-06-27 09:43:57 +0530

    fix db connection to use atlas

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/EVENTMANAGEMENTdb")
+mongoose.connect(process.env.MONGO_URI)
 .then(() => {
     console.log("Database Connected");
 })
